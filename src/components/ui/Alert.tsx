9da93e80491fe37a6929195da575e3f2d13f42ad
Luis Cadillo <luiscaf3r@gmail.com>
2022-11-02 21:33:34 -0500

    fix: <ExclamationTriangleIcon /> not found

--- src/components/ui/Alert.tsx
+++ src/components/ui/Alert.tsx
@@ -1,4 +1,9 @@
-import { CheckCircleIcon, ExclamationIcon, InformationCircleIcon, XCircleIcon } from "@heroicons/react/20/solid";
+import {
+  CheckCircleIcon,
+  ExclamationTriangleIcon,
+  InformationCircleIcon,
+  XCircleIcon,
+} from "@heroicons/react/20/solid";
 import classNames from "classnames";
 import { ReactNode } from "react";
 
@@ -19,17 +24,22 @@ export function Alert(props: AlertProps) {
         "rounded-md border border-opacity-20 p-3",
         props.className,
         severity === "error" && "border-red-900 bg-red-50 text-red-800",
-        severity === "warning" && "border-yellow-700 bg-yellow-50 text-yellow-700",
+        severity === "warning" &&
+          "border-yellow-700 bg-yellow-50 text-yellow-700",
         severity === "info" && "border-sky-700 bg-sky-50 text-sky-700",
         severity === "success" && "bg-gray-900 text-white"
-      )}>
+      )}
+    >
       <div className="relative flex flex-col md:flex-row">
         <div className="flex-shrink-0">
           {severity === "error" && (
-            <XCircleIcon className={classNames("h-5 w-5 text-red-400", iconClassName)} aria-hidden="true" />
+            <XCircleIcon
+              className={classNames("h-5 w-5 text-red-400", iconClassName)}
+              aria-hidden="true"
+            />
           )}
           {severity === "warning" && (
-            <ExclamationIcon
+            <ExclamationTriangleIcon
               className={classNames("h-5 w-5 text-yellow-400", iconClassName)}
               aria-hidden="true"
             />
@@ -51,7 +61,11 @@ export function Alert(props: AlertProps) {
           <h3 className="text-sm font-medium">{props.title}</h3>
           <div className="text-sm">{props.message}</div>
         </div>
-        {props.actions && <div className="absolute top-1 right-1 text-sm md:relative">{props.actions}</div>}
+        {props.actions && (
+          <div className="absolute top-1 right-1 text-sm md:relative">
+            {props.actions}
+          </div>
+        )}
       </div>
     </div>
   );

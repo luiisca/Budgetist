70a6689d92880d5d1c5d35a564330e42aa082efa
Luis Cadillo <luiscaf3r@gmail.com>
2024-03-01 09:56:02 -0500

    simplify zod schemas, we only use one schema for both client and server validation

--- prisma/zod-utils.ts
+++ prisma/zod-utils.ts
@@ -2,156 +2,102 @@ import { z } from "zod";
 
 // helpers
 const nonEmptyString = z.string().min(1, { message: "Cannot be empty" });
+const numberInput = z.number({ invalid_type_error: "Cannot be empty" }).positive() // we're assuming input is only not a number when set to "", useful for <NumberInput />
+const yearFrequency = numberInput.min(1).max(12)
 
-const equalTo = (value: string) => {
-  return z
-    .string()
-    .startsWith(value, {
-      message: `Must be equal to ${value}`,
-    })
-    .endsWith(value, {
-      message: `Must be equal to ${value}`,
-    });
-};
-const range = (init: number, end: number) => z.number().min(init).max(end);
+const range = (init: number, end: number) => numberInput.min(init).max(end);
 
 export const username = z
-  .string()
-  .min(2, { message: "Must be at least 2 characters long" })
-  .optional();
+    .string()
+    .min(2, { message: "Must be at least 2 characters long" })
+    .optional();
 
 const percentage = z
-  .number()
-  .positive()
-  .max(100, { message: "Cannot be greater than 100%" });
+    .number()
+    .positive()
+    .max(100, { message: "Cannot be greater than 100%" });
 
 export const selectOptions = z.object({
-  value: z.string().optional(),
-  label: z.string().optional(),
+    value: z.string().optional(),
+    label: z.string().optional(),
 });
 
 // profile
 export const profileData = z.object({
-  username,
-  name: z.string().optional(),
-  avatar: z.string().optional(),
-  country: z.string().optional(),
-  inflation: z.string().or(z.number().positive()).optional(),
-  currency: z.string().optional(),
-  investPerc: z.string().or(percentage).optional(),
-  indexReturn: z.string().or(z.number().positive()).optional(),
-  completedOnboarding: z.boolean().optional(),
+    username,
+    name: z.string().optional(),
+    image: z.string().optional(),
+    country: z.string().optional(),
+    inflation: z.string().or(z.number().positive()).optional(),
+    currency: z.string().optional(),
+    investPerc: z.string().or(percentage).optional(),
+    indexReturn: z.string().or(z.number().positive()).optional(),
+    completedOnboarding: z.boolean().optional(),
 });
 
 // salary
-export const salaryDataClient = z.object({
-  title: z.string().optional(),
-  currency: selectOptions,
-  amount: z.number().positive(),
-  taxType: selectOptions,
-  taxPercent: nonEmptyString.or(range(0, 100)),
-  variance: z
-    .array(
-      z
-        .object({
-          from: nonEmptyString.or(z.number().positive()),
-          amount: nonEmptyString.or(z.number().positive()),
-          taxPercent: z.string().or(percentage).optional(),
-        })
-        .required()
-    )
-    .optional(),
-});
-export const salaryDataServer = salaryDataClient.extend({
-  id: z.number().positive().optional(),
-  currency: z.string().optional(),
-  taxType: z.union([equalTo("perCat"), equalTo("perRec")]),
-  taxPercent: range(0, 100),
-  variance: z
-    .array(
-      z
-        .object({
-          from: z.number().positive(),
-          amount: z.number().positive(),
-          taxPercent: percentage,
-        })
-        .required()
-    )
-    .optional(),
+const optSalVarianceData = z.object({
+    taxPercent: z.string().or(percentage).optional(),
+})
+const optSalData = z.object({
+    id: z.bigint().positive().optional(),
+    title: z.string().optional(),
+    variance: z
+        .array(
+            optSalVarianceData.extend({
+                from: nonEmptyString.or(z.number().positive()),
+                amount: nonEmptyString.or(z.number().positive()),
+            }).required()
+        )
+        .optional(),
+})
+export const salData = optSalData.extend({
+    currency: selectOptions,
+    amount: numberInput,
+    taxType: selectOptions,
+    taxPercent: range(0, 100),
 });
 
 // categories
-export const categoryDataClient = z.object({
-  id: z.number().optional(),
-  title: nonEmptyString,
-  budget: nonEmptyString.or(z.number().positive()),
-  currency: selectOptions,
-  type: selectOptions,
-  inflType: selectOptions,
-  country: selectOptions,
-  inflVal: nonEmptyString.or(z.number().positive()).optional(), // pass default
-  icon: z.string().optional(), // pass random default
+const optCatData = z.object({
+    id: z.bigint().optional(),
+    inflVal: numberInput.optional(),
+    icon: z.string().optional(),
+    frequency: yearFrequency.optional(),
+})
+const optCatRecordData = z.object({
+    title: z.string().optional(),
+    frequency: yearFrequency.optional(),
+    inflation: numberInput.optional(),
+    currency: selectOptions.optional(),
+})
+export const catData = optCatData.extend({
+    title: nonEmptyString,
+    budget: numberInput,
+    currency: selectOptions,
+    type: selectOptions,
+    inflType: selectOptions,
+    country: selectOptions,
 
-  records: z
-    .array(
-      z
-        .object({
-          title: z.string().optional(),
-          amount: nonEmptyString.or(z.number().positive()),
-          frequency: nonEmptyString.or(z.number().positive()).optional(), // pass default // must be between 1 & 12
-          inflType: z.boolean(),
-          inflation: z.string().or(z.number().positive()).optional(), // parent default
-          type: selectOptions,
-          country: selectOptions,
-          currency: selectOptions.optional(),
-        })
-        .required()
-    )
-    .optional(),
-  freqType: selectOptions,
-  frequency: z.union([nonEmptyString, z.number().positive()]).optional(), // pass default
-});
-export const categoryDataServer = categoryDataClient.extend({
-  id: z.number().positive().optional(),
-  budget: z.number().positive(),
-  currency: z.union([equalTo("perRec"), z.string().optional()]), // pass default
-  type: z.union([equalTo("income"), equalTo("outcome")]),
-  inflType: z.union([equalTo(""), equalTo("perCat"), equalTo("perRec")]),
-  country: z.string(),
-  inflVal: z.number().positive(),
-  icon: z.string(),
-
-  records: z
-    .array(
-      z
-        .object({
-          title: z.string().optional(),
-          amount: z.number().positive(),
-          frequency: z.number().positive(),
-          inflType: z.boolean(),
-          inflation: z.number().positive(),
-          type: z.union([equalTo("income"), equalTo("outcome")]),
-          country: z.string(),
-          currency: z.string(),
-        })
-        .required()
-    )
-    .optional(),
-  freqType: z.union([equalTo("perRec"), equalTo("perCat")]),
-  frequency: z.number().positive(),
+    records: z
+        .array(
+            optCatRecordData.extend({
+                amount: numberInput,
+                inflType: z.boolean(),
+                type: selectOptions,
+                country: selectOptions,
+            }).required()
+        )
+        .optional(),
+    freqType: selectOptions,
 });
 
 // run simulation
 export const runSimulationData = z.object({
-  years: nonEmptyString.or(z.number().positive()),
+    years: nonEmptyString.or(z.number().positive()),
 });
 
 export type ProfileDataInputType = z.infer<typeof profileData>;
-
-export type SalaryDataInputTypeClient = z.infer<typeof salaryDataClient>;
-export type SalaryDataInputTypeServer = z.infer<typeof salaryDataServer>;
-
-export type CategoryDataInputTypeClient = z.infer<typeof categoryDataClient>;
-export type CategoryDataInputTypeServer = z.infer<typeof categoryDataServer>;
-
+export type SalInputDataType = z.infer<typeof salData>;
+export type CatInputDataType = z.infer<typeof catData>;
 export type RunSimulationDataType = z.infer<typeof runSimulationData>;
